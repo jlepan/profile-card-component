e00752275074cddb9ba500365e07f4d83a51c55f
jlepan <juliette.lepan@hotmail.fr>
2025-11-25 17:36:22 +0100

    Update base on vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/browser-extensions-manager-ui-main/",
+  base: "/profile-card-component/",
   build: {
     rollupOptions: {
       input: {
